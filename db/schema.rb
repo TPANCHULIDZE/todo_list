1f91b2fd860e8f9d77daebe69c172339bc034cf4
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-05-19 09:45:02 +0400

    delete

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_103243) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "is_end", default: false
   end
 
   create_table "users", force: :cascade do |t|
